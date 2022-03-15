ceb85de147c9a41160a4382634a8acc1eee53fe1
SejalNikumbh <sejalnikumbh14@gmail.com>
2022-03-15 15:06:09 +0530

    MODIFIED: Daily Wage Calulation

--- src/com/InfoGalaxy/EmployeeWageBuilder.java
+++ src/com/InfoGalaxy/EmployeeWageBuilder.java
@@ -4,19 +4,20 @@ import java.util.Random;
 
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-    //UC_1: checkAttendeance
+        //UC_2: checkAttendeance
+        final int WAGE_PER_HR = 20;
+        final int FULL_DAY_HR = 8;
+        int total_wage = 0;
+        //UC-1: checkAttendeance
         Random random = new Random();
-        int checkAttendeance = random.nextInt()%2;{
-            System.out.println(("Attendance ="+checkAttendeance));
-        }
+        int checkAttendeance = random.nextInt() % 2;
         if (checkAttendeance == 0) {
             System.out.println("Employee is Absent.");
-        } else if(checkAttendeance == 1) {
+        } else {
             System.out.println("Employee is Present.");
-
+            total_wage = FULL_DAY_HR * WAGE_PER_HR;
         }
-        else{
-            System.out.println(("Employee is Invalid"));
+        System.out.println("Total wage = "+ total_wage);
         }
     }
-}
+
