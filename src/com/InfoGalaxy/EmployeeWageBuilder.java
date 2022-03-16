d6a3e47960ad51da6574e9ee43913c174119340a
SejalNikumbh <sejalnikumbh14@gmail.com>
2022-03-16 14:56:37 +0530

    MODIFIED: check Employee type

--- src/com/InfoGalaxy/EmployeeWageBuilder.java
+++ src/com/InfoGalaxy/EmployeeWageBuilder.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         //UC_2: checkAttendeance
+        //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
         final int WAGE_PER_HR = 20;
         final int FULL_DAY_HR = 8;
         final int PART_TIME_HR = 4;
@@ -17,13 +18,16 @@ public class EmployeeWageBuilder {
         } else {
             System.out.println("Employee is Present.");
             int empType = random.nextInt()%2;
-            if(empType == 0) {
-                System.out.println("Employee is part time");
-                total_wage = PART_TIME_HR * WAGE_PER_HR;
-            }
-            else{
-                System.out.println("Employee is Full time");
-                total_wage = FULL_DAY_HR * WAGE_PER_HR;
+            switch(empType) {
+                case 0:
+                    System.out.println("Employee is part time");
+                    total_wage = PART_TIME_HR * WAGE_PER_HR;
+                    break;
+                case  1:
+                case -1:
+                    System.out.println("Employee is Full time");
+                    total_wage = FULL_DAY_HR * WAGE_PER_HR;
+                break;
             }
         }
         System.out.println("Total wage = "+ total_wage);
